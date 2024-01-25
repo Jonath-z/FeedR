2fe96134b4af4dfd92478735b357cb7091927426
Jonath-z <jonathanzihindula95@gmail.com>
2024-01-25 14:08:34 +0200

    feat: add zerodev for account abstraction

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,8 @@
 import { connectUTU } from "./lib/web3/utu";
 import { DynamicContextProvider, DynamicWidget } from "@dynamic-labs/sdk-react-core";
-// import { DynamicWagmiConnector } from "@dynamic-labs/wagmi-connector";
 
 import { EthereumWalletConnectors } from "@dynamic-labs/ethereum";
+import { ZeroDevSmartWalletConnectors } from "@dynamic-labs/ethereum-aa";
 
 function App() {
   return (
@@ -10,16 +10,19 @@ function App() {
       settings={{
         // Find your environment id at https://app.dynamic.xyz/dashboard/developer
         environmentId: process.env.REACT_APP_DYNAMIC_LAB_ENVIRONMENT_ID ?? "",
-        walletConnectors: [EthereumWalletConnectors],
+        walletConnectors: [EthereumWalletConnectors, ZeroDevSmartWalletConnectors],
         eventsCallbacks: {
           onAuthSuccess: async ({ primaryWallet }) => {
-            console.log({ primaryWallet });
+            if (primaryWallet) {
+              await connectUTU();
+            }
+          },
+          onEmbeddedWalletCreated: async () => {
             await connectUTU();
           },
         },
       }}
     >
-      {/*<DynamicWagmiConnector>*/}
       <div className="w-full h-screen flex flex-col justify-center gap-5 items-center text-5xl">
         Welcome to FeedR
         <button onClick={connectUTU} className="text-lg  mt-5">
@@ -27,7 +30,6 @@ function App() {
         </button>
         <DynamicWidget />
       </div>
-      {/*</DynamicWagmiConnector>*/}
     </DynamicContextProvider>
   );
 }
